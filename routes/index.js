8a80c9fc817875b0fa6beaf67ec8c2e268297f74
mbareeva <beeemaja@gmail.com>
2021-05-08 21:57:16 +0200

    routes changed

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/handleauth', async (req, res) => {
   try {
     const code = req.query.code;
     const data = await instagram.authorizeUser(code, redirectURi);
-      req.session.access_token = 'IGQVJXX01fTXBwWlpCWHF0dmF3VC1ZAbUczbnctS0M5TVlOU1ZASQ0ZAnZAWx4NEZAqeUl4bUFLOWE3Q3NaT3o4bHl3MWJ1RExZAbnB5UWRYbnlkS0cxZAGQ2a0lJaGZASeUVxTGNsTkx5TGVyaUMtU2VyaVYxaQZDZD';
+      req.session.access_token = data.access_token;
       req.session.user_id = data.user_id;
       console.log("user id: " + data.access_token);
       instagram.config.accessToken = req.session.access_token;
@@ -45,8 +45,10 @@ router.get('/logout', () => {})
 
 router.get('/profile', async(req, res) => {
   try {
-    const profileData = await instagram.get('users/self');
-    const media = await instagram.get('users/self/media/recent');
+    //const profileData = await instagram.get('users/self');
+    console.log("token: " + req.session.access_token);
+    const media = await instagram.get('users/:user-id/media/recent', {access_token: req.session.access_token});
+    const profileData = await instagram.get('users/:user-id', {access_token: req.session.access_token});
     res.render('profile', {user: profileData.data, posts: media.data});
   } catch(e) {
     console.log(e);
