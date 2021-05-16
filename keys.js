9ec09bd67ac9eafcd9e265ee67bde2560b637421
mbareeva <beeemaja@gmail.com>
2021-05-16 23:58:49 +0200

    keys deleted

--- keys.js
+++ keys.js
@@ -1,13 +1,6 @@
 module.exports = {
   instagram: {
     clientId: '457381115518080',
-    clientSecret: '8220df728b96cc570c4d0609d66b6028',
-    testUsers: [
-      {
-        username: 'mayabareeva',
-        accessToken: 'IGQVJYX3VRX3VHVEZAaQW92NXNSeU5jV01RYWp3VnJGUGZAXbGJuOGdqemxrbEZATMS1IU1FYcHBjN0k3V2tVREVyVWQ2eF9BSUtXS3dPcEd4YnBDR3IzUG4xOHF3cmxMcXhsYVdvbW94SUNLZAjhGSjNKaAZDZD'
-      }
-    ]
-
+    clientSecret: '8220df728b96cc570c4d0609d66b6028'
   }
 };
